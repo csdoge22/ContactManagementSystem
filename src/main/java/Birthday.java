eb66eabea4dab76e028551156193e93ba5605661
csdoge22 <sunnyambashta@gmail.com>
2023-10-08 19:10:05 -0400

    Birthday class

--- src/main/java/Birthday.java
+++ src/main/java/Birthday.java
@@ -1,4 +1,16 @@
-
+import java.util.Date;
 public class Birthday {
-
+	private int month, day, year, curMonth, curDay, curYear, age;
+	private Date currentDate;
+	
+	public Birthday(int m, int d, int y) {
+		month = m;
+		day = d;
+		year = y;
+		currentDate = new Date();
+	}
+	
+	public int getAge() {
+		
+	}
 }
