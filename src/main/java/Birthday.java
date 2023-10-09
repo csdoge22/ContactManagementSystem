4d866f963ddd4511e06ab30ab53d6020cc579999
csdoge22 <sunnyambashta@gmail.com>
2023-10-08 20:34:19 -0400

    Complete implementation of the Birthday and Email Classes

--- src/main/java/Birthday.java
+++ src/main/java/Birthday.java
@@ -1,16 +1,39 @@
 import java.util.Date;
 public class Birthday {
-	private int month, day, year, curMonth, curDay, curYear, age;
-	private Date currentDate;
+	private int month, day, year;
 	
 	public Birthday(int m, int d, int y) {
 		month = m;
 		day = d;
 		year = y;
-		currentDate = new Date();
 	}
-	
-	public int getAge() {
-		
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	@Override
+	public String toString() {
+		return "Birthday [month=" + month + ", day=" + day + ", year=" + year + "]";
 	}
 }
